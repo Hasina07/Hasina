4d076db9444d776bdcd1738e95ecf054f58fd79a
khani <khani@DESKTOP-VUQ5CR2>
2022-02-10 15:43:53 +0530

    3 files

---
+++ mit-Hasina/src/Hasina/Looping1.java
@@ -0,0 +1,28 @@
+package Hasina;
+
+public class Looping1 {
+public static void main(String[] args) {
+//	while(true) {// pre condition check
+//		System.out.println("infinit looppp");
+//	}
+	
+//	do {
+//		System.out.println("infinite do while loop");
+//	}while(true);// post conditionaz check
+	
+	int i=10;
+	while(i>0) {
+		System.out.println("the value of i is...:"+i);
+		i--;
+	}
+	
+//	do {
+//		System.out.println("loop is still executed....");
+//	}while(i>10);
+	
+	do {
+		System.out.println("do while i value is...:"+i);
+		i++;
+	}while(i<10);
+}
+}
---
+++ mit-Hasina/src/Hasina/looping.java
@@ -0,0 +1,13 @@
+package Hasina;
+
+public class looping {
+	public static void main(String[] args) {
+//		for(;;) {
+//			System.out.println("infinite loop");
+//		}
+		
+		for(int i=10;i>0;i--) {
+			System.out.println("the loop executes...:"+i);
+		}
+	}
+}
